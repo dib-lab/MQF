5417584ee2ba9674933b647e5e36ed39b652b14a
Luiz Irber <luiz.irber@gmail.com>
2019-11-03 17:29:49 -0800

    start rust API, move raw FFI into another module

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,19 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+mod raw;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+use std::ptr;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::ffi::CString;
-    use std::ptr;
+pub struct MQF {
+    inner: raw::QF,
+}
 
-    #[test]
-    fn simple_counting_test() {
-        //except first item is inserted 5 times to full test _insert1
-        let mut qf: QF = QF {
-            mem: ptr::null_mut(),
-            metadata: ptr::null_mut(),
-            blocks: ptr::null_mut(),
-        };
-
-        let counter_size = 2;
-        let qbits = 5;
-        let num_hash_bits = qbits + 8;
-        let maximum_count = (1u64 << counter_size) - 1;
-        let mut count = 0;
-        let mut fixed_counter = 0;
-
-        let s = CString::new("").unwrap();
-
-        //INFO("Counter size = "<<counter_size<<" max count= "<<maximum_count);
-        unsafe {
-            qf_init(
-                &mut qf,
-                1u64 << qbits,
-                num_hash_bits,
-                0,
-                counter_size,
-                0,
-                true,
-                s.as_ptr(),
-                2038074761,
-            );
+impl Default for MQF {
+    fn default() -> MQF {
+        MQF {
+            inner: raw::QF {
+                mem: ptr::null_mut(),
+                metadata: ptr::null_mut(),
+                blocks: ptr::null_mut(),
+            },
         }
-
-        for i in 0..=10 {
-            unsafe {
-                qf_insert(&mut qf, 100, 1, false, false);
-            };
-            count = unsafe { qf_count_key(&qf, 100) };
-            dbg!((count, fixed_counter));
-            assert_eq!(count, 1 + i);
-        }
-
-        unsafe {
-            qf_insert(&mut qf, 1500, 50, false, false);
-        }
-
-        count = unsafe { qf_count_key(&qf, 1500) };
-        dbg!((count, fixed_counter));
-        assert_eq!(count, 50);
-
-        unsafe {
-            qf_insert(&mut qf, 1600, 60, false, false);
-        }
-        count = unsafe { qf_count_key(&qf, 1600) };
-        dbg!((count, fixed_counter));
-        assert_eq!(count, 60);
-
-        unsafe {
-            qf_insert(&mut qf, 2000, 4000, false, false);
-        }
-        count = unsafe { qf_count_key(&qf, 2000) };
-        dbg!((count, fixed_counter));
-        assert_eq!(count, 4000);
     }
 }
